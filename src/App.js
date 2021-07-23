8491ed52b4acb272dc67003c70927d17646a2ea8
sunny-lee3 <sunnylee3712@gmail.com>
2021-07-22 19:37:07 -0700

    reducer functions added, still not complete. Need to map history and add button to delete history

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Library imports 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useReducer } from 'react';
 import axios from 'axios';
 //  Styling imports 
 import './app.scss';
@@ -9,17 +9,54 @@ import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
+import History from './components/History/history.js';
 
+
+const initialState = {
+    history: [],
+    active: {}
+}
+
+//useReducer() state for history 
+const reducer = (stateHistory, action) => {
+
+    switch(action.type){
+        case 'ADD_HISTORY':
+            return {...stateHistory, history: [...stateHistory.history, action.payload]}
+        case 'DELETE_HISTORY': 
+            return {...stateHistory, history: stateHistory.history.filter(hist => hist.requests !== action.payload)}
+        case 'ACTIVATE_HISTORY': 
+            return {...stateHistory, history: stateHistory.history.filter(hist => hist.requests === action.payload)}
+        default:
+            return stateHistory;
+    }
+}
+
+const addHistory = (history) => {
+    return {
+        type: 'ADD_HISTORY',
+        payload: history,
+    }
+}
+const deleteHistory = (requests) => {
+    return {
+        type: 'DELETE_HISTORY',
+        payload: requests,
+    }
+}
 
 function FunctionalApp() {
     // State variable and set state of that variable 
     const [data, setData] = useState("");
     const [reqParams, setReqParams] = useState("");
-    
-    // Function that will be used as mock data 
+    //state for reducer
+    const [stateHistory, dispatch] = useReducer(reducer, initialState);
+
+    // useEffect is used to call api or to url that user submits on form.
 useEffect(() => {
     if(Object.keys(reqParams).length) axios(reqParams).then(res => {
         setData(res)
+        dispatch(addHistory({request: res}))
     });
     console.log('reqParams from App', reqParams);
 },[reqParams]);
@@ -32,8 +69,8 @@ useEffect(() => {
             <div>URL: {reqParams.url}</div>
             {/* function is passed as props in the Form component */}
             <Form setReqParams={setReqParams} />
-
             <Results data={data}  />
+            <History history={stateHistory}/> 
             <Footer />
         </React.Fragment>
     )
